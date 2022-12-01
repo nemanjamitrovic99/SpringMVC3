74bfe298b3c35719f3b30d7735589074ff43bb7d
Nemanja Mitrovic <nemanja.mitrovic@en510092.endava.net>
2022-12-01 14:12:20 +0100

    Pocetni projekat

---
+++ src/main/java/com/pluralsight/conferenceMVC1/ConferenceMvc1Application.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferenceMVC1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceMvc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceMvc1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluralsight/conferenceMVC1/ConferenceMvc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferenceMVC1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceMvc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
