83573795e8722e2ec9b516f37391a914bf398b61
Nemanja Mitrovic <nemanja.mitrovic@en510092.endava.net>
2022-12-02 17:04:17 +0100

    Demo: Passing Parameters Dodat model i ModelAttribute iskoriscen, sada radi i POST

--- src/main/java/com/pluralsight/conferenceMVC1/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/conferenceMVC1/controller/RegistrationController.java
@@ -4,6 +4,7 @@ import com.pluralsight.conferenceMVC1.model.Registration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
 
@@ -14,5 +15,12 @@ public class RegistrationController {
     public String getRegistration(@ModelAttribute("registration")Registration registration){
         //ovo registration iz ModelAttribute se vezuje za registration iz registration.jsp stranice u tagu form
         return "registration";          //i ovo mora da bude registration
+    }
+
+    @PostMapping("registration")
+    public String addRegistration(@ModelAttribute("registration")Registration registration){
+        System.out.println("Registration: "+registration.getName());
+
+        return "registration";
     }
 }
