cd1fbc32415f2c806098460ffa70101c8fc45519
Nemanja Mitrovic <nemanja.mitrovic@en510092.endava.net>
2022-12-05 14:30:50 +0100

    Demo: jQuery Integration

--- src/main/java/com/pluralsight/conferenceMVC1/controller/UserController.java
+++ src/main/java/com/pluralsight/conferenceMVC1/controller/UserController.java
@@ -2,6 +2,7 @@ package com.pluralsight.conferenceMVC1.controller;
 
 import com.pluralsight.conferenceMVC1.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +12,19 @@ public class UserController {
     @GetMapping("/user")
     public User getUser(@RequestParam(value = "firstname", defaultValue = "Nemanja")String firstname,
                         @RequestParam(value = "lastname", defaultValue = "Mitrovic")String lastname,
-                        @RequestParam(value = "age", defaultValue = "22")int age){
+                        @RequestParam(value = "age", defaultValue = "26")int age){
         User user=new User();
 
         user.setFirstname(firstname);
         user.setLastname(lastname);
         user.setAge(age);
+
+        return user;
+    }
+
+    @PostMapping("/user")
+    public User postUser(User user){
+        System.out.println("User firstname:"+user.getFirstname());
 
         return user;
     }
