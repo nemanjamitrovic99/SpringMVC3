af3c7210c7e94c2bbb52b14c23305c9fc8472f29
Nemanja Mitrovic <nemanja.mitrovic@en510092.endava.net>
2022-12-05 10:24:16 +0100

    Post-Redirect-Get_clean text field on submit

--- src/main/java/com/pluralsight/conferenceMVC1/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/conferenceMVC1/controller/RegistrationController.java
@@ -21,6 +21,6 @@ public class RegistrationController {
     public String addRegistration(@ModelAttribute("registration")Registration registration){
         System.out.println("Registration: "+registration.getName());
 
-        return "registration";
+        return "redirect:registration";
     }
 }
