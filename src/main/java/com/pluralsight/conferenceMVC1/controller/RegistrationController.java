201e9309300c304ce9e14f4af859cb9cd1d576a9
Nemanja Mitrovic <nemanja.mitrovic@en510092.endava.net>
2022-12-05 13:22:34 +0100

    Demo: Validation

--- src/main/java/com/pluralsight/conferenceMVC1/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/conferenceMVC1/controller/RegistrationController.java
@@ -2,10 +2,12 @@ package com.pluralsight.conferenceMVC1.controller;
 
 import com.pluralsight.conferenceMVC1.model.Registration;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
@@ -18,7 +20,13 @@ public class RegistrationController {
     }
 
     @PostMapping("registration")
-    public String addRegistration(@ModelAttribute("registration")Registration registration){
+    public String addRegistration(@Valid @ModelAttribute("registration")Registration registration,
+                                  BindingResult result){
+        if(result.hasErrors()){
+            System.out.println("There were errors");
+            return "registration";
+        }
+
         System.out.println("Registration: "+registration.getName());
 
         return "redirect:registration";
--- src/main/webapp/WEB-INF/jsp/registration.jsp
+++ src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -4,10 +4,24 @@
 <html>
 <head>
     <title>Registration</title>
+    <style>
+        .error{
+            color: #ff0000;
+        }
+
+        .errorblock{
+            color: #000;
+            background-color: #ffEEEE;
+            border: 3px solid #ff0000;
+            padding: 8px;
+            margin: 16px;
+        }
+    </style>
 </head>
 <body>
     <h1>Registration</h1>
     <form:form modelAttribute="registration">
+        <form:errors path="*" cssClass="errorblock" element="div" />
         <table>
             <tr>
                 <td>
@@ -22,7 +36,9 @@
                     <input type="submit" value="Add Registration">
                 </td>
             </tr>
+
         </table>
+
     </form:form>
 </body>
 </html>
